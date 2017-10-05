89ccc3acd80af73cbf501bc22a398def732028ad
Ryan Morrison <ryanam26@yahoo.com>
2017-10-05 11:13:58 -0400

    completed exercise 12

---
+++ 12_function_parameter_defaults.py
@@ -0,0 +1,12 @@
+def simple(num1, num2=5):  #num2=5 is default
+	print(num1, num2)
+
+simple(2)
+
+
+def basic_window(width, height, font='TNR',
+				bgcolor='w', scollbar = True):
+
+	print(width, height, font,bgcolor)
+
+basic_window(500,350)
